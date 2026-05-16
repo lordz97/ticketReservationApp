871cd2b7d8007d61956c650d7f058bd97c6e4730
Harisson Friedman Zeufack <87904508+lordz97@users.noreply.github.com>
2026-05-16 17:59:07 +0200

    Delete frontend directory

--- frontend/src/main/java/org/example/ticketReservation/domain/ResourceType.java
+++
@@ -1,7 +0,0 @@
-package org.example.ticketReservation.domain;
-
-public enum ResourceType {
-    TRAIN_SEAT,
-    ROOM,
-    DESK
-}
--- frontend/src/main/java/org/example/ticketReservation/exception/InvalidReservationException.java
+++
@@ -1,7 +0,0 @@
-package org.example.ticketReservation.exception;
-
-public class InvalidReservationException extends RuntimeException{
-    public InvalidReservationException(String message){
-        super(message);
-    }
-}
